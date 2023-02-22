2216836dd10c0e9a1912aba67f9d0396051d044e
shivansh0410 <118688027+shivansh0410@users.noreply.github.com>
2023-02-23 01:46:21 +0530

    Add files via upload

---
+++ React js & Linked Lists/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello,</h1> 
+      <h2>My Name Is Shivansh</h2>
+      <p>Welcome to my React application.</p>
+    </div>
+  );
+}
+
+export default App;
+
